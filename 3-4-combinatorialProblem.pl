234dfecb76ecf3d31dfb9614861c5e442985b08a
eslerkang <eslerkang@gmail.com>
2023-05-01 07:33:51 +0000

    :)

--- 3-4-combinatorialProblem.pl
+++ 3-4-combinatorialProblem.pl
@@ -5,3 +5,5 @@
 % sat(((~D)+(~B)+C)*(B+(~A)+(~C))*((~C)+(~B)+E)*(E+(~D)+B)*(B+E+(~C))), labeling([A,B,C,D,E]).
 % sat(~A*A).
 % sat((~A+B)*A*(~B)).
+
+solution([A1, A2, A3, A4, A5, A6]) :- sat(A1 =:= A2*A3*A4*A5*A6), sat(A2 =:= ~(A3+A4+A5+A6)), sat(A3 =:= A1*A2), sat(A4 =:= A1+A2+A3), sat(A5 =:= ~(A1+A2+A3+A4)), sat(A6 =:= ~(A1+A2+A3+A4+A5)).
